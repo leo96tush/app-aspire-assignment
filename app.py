de354189e731b89ac7173915706d51b5278ab987
tushargupta5 <tushar.gupta5@delhivery.com>
2025-01-05 15:17:25 +0530

    Adding files

--- app.py
+++ app.py
@@ -3,16 +3,24 @@ from flask import (
     Flask,
     jsonify
 )
+from flask_pymongo import PyMongo
 from http import HTTPStatus
 
+
+# Import Settings
 from settings import get_config
 
+# Creating Flask App
 app = Flask(__name__)
 logger = logging.getLogger(__name__)
 
-# Load configuration based on the FLASK_ENV environment variable
+# Load configuration dynamically
 Config = get_config()
 app.config.from_object(Config)
+
+# Connect to MongoDB
+mongo = PyMongo(app)
+db = mongo.db
 
 @app.route('/ping', methods=['GET'])
 def health_check():
